c4d5f9fef95507cec816f92529add1901f77d886
r-ush <aarush.bhatt@gmail.com>
2020-05-24 14:18:20 +0530

    added credibility

--- src/components/news-card/news-card.jsx
+++ src/components/news-card/news-card.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './news-card.css'
 
+function getGreenToRed(percent){
+    var r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+    var g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+    return 'rgb('+r+','+g+',0)';
+}
+
 class NewsCard extends React.Component{
     render() {
-        const {title, image,description,time, sourceName, sourceUrl}=this.props
+        const {title, image,description,time, sourceName, sourceUrl,credibility=50}=this.props
         return(
             <div className='news-card'>
                 <img className='news-img' src={image} alt='news-img' />
@@ -13,6 +19,7 @@ class NewsCard extends React.Component{
                     <div className='news-card-footer'>
                         {time}<br/><br/>
                         <a rel="noopener noreferrer"  target='_blank' href={sourceUrl}>View here at {sourceName}</a>
+                        <p style={{color:getGreenToRed(credibility)}}>This article is {credibility}% credible</p>
                     </div>
                 </div>
             </div>
